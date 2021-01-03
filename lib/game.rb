294f37e54ce68af217e9876b8be7a0bc4fbee712
Brian Firestone <bfirestone2339@gmail.com>
2021-01-03 16:07:57 -0500

    commit tree 6fd83519318ce435078d88afbc905090dd56562c

---
+++ lib/game.rb
@@ -0,0 +1,76 @@
+require 'pry'
+class Game 
+    attr_accessor :board, :player_1, :player_2
+
+    WIN_COMBINATIONS = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [6,4,2]
+    ]
+
+    def initialize(player_1=Players::Human.new("X"), player_2=Players::Human.new("O"), board=Board.new)
+        @player_1 = player_1
+        @player_2 = player_2
+        @board = board
+    end
+
+    def current_player
+        board.turn_count % 2 == 0 ? player_1 : player_2
+    end
+
+    def won?
+        result = WIN_COMBINATIONS.detect do |combo|
+            board.cells[combo[0]] == board.cells[combo[1]] &&
+            board.cells[combo[1]] == board.cells[combo[2]] &&
+            board.taken?(combo[0]+1)
+        end
+    end
+
+    def draw?
+        board.full? && !won?
+    end
+
+    def over?
+        draw? || won?
+    end
+
+    def winner
+        winning_combo = won? 
+        i = nil
+        if winning_combo
+            # binding.pry
+            i = @board.cells[winning_combo.first]
+        end
+        i 
+    end
+
+    def turn
+        player = current_player
+        move = player.move(board)
+        if !board.valid_move?(move) 
+            turn
+        else
+            board.update(move, player)
+            # board.display
+        end
+    end
+
+    def play
+        while !over?
+            turn
+        end
+        if won?
+            # binding.pry
+            puts "Congratulations #{winner}!"
+        elsif draw?
+            puts "Cat's Game!"
+        end
+    end
+
+
+end
